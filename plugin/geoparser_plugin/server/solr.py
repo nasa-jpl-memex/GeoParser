d3bb50d0ff9bf37761b84e695f8d8828271bf5b9
Maziyar Boustani <boustani@jpl.nasa.gov>
2015-10-21 10:40:04 -0700

    Index latitude and longitude into Solr

--- plugin/geoparser_plugin/server/solr.py
+++ plugin/geoparser_plugin/server/solr.py
@@ -2,7 +2,7 @@ import os
 import urllib2
 
 SOLR_URL = "http://localhost:8983/solr/"
-COLLECTION_NAME = "uploaded_files"
+COLLECTION_NAME = "uploaded_files3"
 
 
 def check_solr():
@@ -93,7 +93,7 @@ def IndexLocationName(file_name, places):
     if create_core(COLLECTION_NAME):
         try:
             p = ",".join(places)
-            url = SOLR_URL + COLLECTION_NAME + '/update?stream.body=[{%22id%22:%22' + file_name + '%22,%22places%22:{%22set%22:"' + p + '"}}]&commit=true'
+            url = SOLR_URL + COLLECTION_NAME + '/update?stream.body=[{%22id%22:%22' + file_name + '%22,%22locations%22:{%22set%22:"' + p + '"}}]&commit=true'
             urllib2.urlopen(url)
             return (True, "Location name/s indexed to Solr successfully.")
         except:
@@ -106,15 +106,27 @@ def QueryLocationName(file_name):
     '''
     Return location names for given filename
     '''
+    if create_core(COLLECTION_NAME):
+        try:
+            url = '{0}{1}/select?q=*%3A*&fq=id%3A%22{2}%22&wt=json&indent=true'.format(SOLR_URL, COLLECTION_NAME, file_name)
+            response = urllib2.urlopen(url)
+            return eval(response.read())['response']['docs'][0]['locations'][0].split(',')
+        except:
+            return False
 
 
 def IndexLatLon(file_name, points):
     '''
     Index points for given file
     '''
-
-
-def QueryPoints(file_name):
-    '''
-    Return geopoints for given filename
-    '''
+    if create_core(COLLECTION_NAME):
+        try:
+            points = ",".join(str(p) for p in points)
+            points = urllib2.quote(points)
+            url = SOLR_URL + COLLECTION_NAME + '/update?stream.body=[{%22id%22:%22' + file_name + '%22,%22points%22:{%22set%22:%22' + points + '%22}}]&commit=true'
+            urllib2.urlopen(url)
+            return (True, "Latitude and longitude indexed to Solr successfully.")
+        except:
+            return (False, "Cannot index latitude and longitude to Solr.")
+    else:
+        return (False, "Either Solr not running or cannot create Core.")
