a9bf9ef30ba54cf209eb8e85b0250d653d905618
smadha <msharan@usc.edu>
2015-12-11 22:52:28 +0530

    Fixing json string being generated.
    Fixed - {u’mr': [u'http://crawl.dyndns.org/solr/materials_research_v2']}
    
    to
    {'mr': ['http://crawl.dyndns.org/solr/materials_research_v2']}

--- geoparser_app/views.py
+++ geoparser_app/views.py
@@ -68,7 +68,7 @@ def list_of_domains(request):
             all_cores.remove("uploaded_files")
         for core in all_cores:
             ids = get_domains_urls(core)
-            domains[core] = ids
+            domains["{0}".format(core)] = ids
     return HttpResponse(status=200, content=str(domains))
 
 
