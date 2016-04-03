347a55fa5a5638a5f28aed2ff4f879c2b8b8777e
smadha <msharan@usc.edu>
2016-04-03 15:29:44 -0700

    fix #65 as per openlayer3 docs

--- geoparser_app/static/js/app.js
+++ geoparser_app/static/js/app.js
@@ -71,7 +71,7 @@ $(function() {
 			{
 				source : new ol.source.XYZ(
 						{
-							url : 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
+							url : 'http://{a-c}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',
 							attributions : [ new ol.Attribution(
 									{
 										html : [ '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>' ]
